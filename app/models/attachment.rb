1a4480b8ee217d54532c01cfd295e39e2a53687f
Joshua Wilborn <Joshua.J.Wilborn@gmail.com>
2013-09-11 17:30:28 -0700

    alphabatize attachment.rb functions, break off lines past 80 colums for readability

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -8,15 +8,18 @@ class Attachment < ActiveRecord::Base
   belongs_to :author, :class_name => "User", :foreign_key => "author_id"
 
   validates_presence_of :filename, :author
-  validates_length_of :filename, :maximum => 255
-  validates_length_of :disk_filename, :maximum => 255
+  validates_length_of   :filename, :maximum => 255
+  validates_length_of   :disk_filename, :maximum => 255
 
   after_validation :put_to_s3
   before_destroy   :delete_from_s3
 
 
   acts_as_event :title => :filename,
-                :url => Proc.new {|o| {:controller => 'attachments', :action => 'download', :id => o.id, :filename => o.filename}}
+                :url => Proc.new {|o| {:controller => 'attachments',
+                                       :action => 'download',
+                                       :id => o.id, 
+                                       :filename => o.filename}}
 
   cattr_accessor :storage_path
   unloadable # Send unloadable so it will not be unloaded in development
@@ -24,47 +27,11 @@ class Attachment < ActiveRecord::Base
 
   @@storage_path = "#{RAILS_ROOT}/files"
 
-  def validate
-    if self.filesize > Setting.attachment_max_size.to_i.kilobytes
-      errors.add(:base, :too_long, :count => Setting.attachment_max_size.to_i.kilobytes)
-    end
+  # Deletes file on the disk
+  def after_destroy
+    File.delete(diskfile) if !filename.blank? && File.exist?(diskfile)
   end
-
-  def put_to_s3
-    if @temp_file && (@temp_file.size > 0)
-      logger.debug("Uploading to #{RedmineS3::Connection.uri}/#{disk_filename}")
-      RedmineS3::Connection.put(disk_filename, @temp_file.read)
-      RedmineS3::Connection.publicly_readable!(disk_filename)
-      md5 = Digest::MD5.new
-      self.digest = md5.hexdigest
-    end
-    @temp_file = nil # so that the model's original after_save block skips writing to the fs
-  end
-
-  def delete_from_s3
-    if ENV['RACK_ENV'] == 'production'
-      logger.debug("Deleting #{RedmineS3::Connection.uri}/#{disk_filename}")
-      RedmineS3::Connection.delete(disk_filename)
-    end
-  end
-
-
-  def file=(incoming_file)
-    unless incoming_file.nil?
-      @temp_file = incoming_file
-      if @temp_file.size > 0
-        self.filename = sanitize_filename(@temp_file.original_filename)
-        self.disk_filename = Attachment.disk_filename(filename)
-        self.content_type = @temp_file.content_type.to_s.chomp
-        self.filesize = @temp_file.size
-      end
-    end
-  end
-
-  def file
-    nil
-  end
-
+  
   # Copies the temporary file to its final location
   # and computes its MD5 hash
   def before_save
@@ -87,9 +54,15 @@ class Attachment < ActiveRecord::Base
     end
   end
 
-  # Deletes file on the disk
-  def after_destroy
-    File.delete(diskfile) if !filename.blank? && File.exist?(diskfile)
+  def deletable?(user=User.current)
+    container.attachments_deletable?(user)
+  end
+
+  def delete_from_s3
+    if ENV['RACK_ENV'] == 'production'
+      logger.debug("Deleting #{RedmineS3::Connection.uri}/#{disk_filename}")
+      RedmineS3::Connection.delete(disk_filename)
+    end
   end
 
   # Returns file's location on disk
@@ -97,6 +70,26 @@ class Attachment < ActiveRecord::Base
     "#{@@storage_path}/#{self.disk_filename}"
   end
 
+  def file
+    nil
+  end
+
+  def file=(incoming_file)
+    unless incoming_file.nil?
+      @temp_file = incoming_file
+      if @temp_file.size > 0
+        self.filename = sanitize_filename(@temp_file.original_filename)
+        self.disk_filename = Attachment.disk_filename(filename)
+        self.content_type = @temp_file.content_type.to_s.chomp
+        self.filesize = @temp_file.size
+      end
+    end
+  end
+  
+  def image?
+    self.filename =~ /\.(jpe?g|gif|png)$/i
+  end
+  
   def increment_download
     increment!(:downloads)
   end
@@ -105,16 +98,15 @@ class Attachment < ActiveRecord::Base
     container.project
   end
 
-  def visible?(user=User.current)
-    container.attachments_visible?(user)
-  end
-
-  def deletable?(user=User.current)
-    container.attachments_deletable?(user)
-  end
-
-  def image?
-    self.filename =~ /\.(jpe?g|gif|png)$/i
+  def put_to_s3
+    if @temp_file && (@temp_file.size > 0)
+      logger.debug("Uploading to #{RedmineS3::Connection.uri}/#{disk_filename}")
+      RedmineS3::Connection.put(disk_filename, @temp_file.read)
+      RedmineS3::Connection.publicly_readable!(disk_filename)
+      md5 = Digest::MD5.new
+      self.digest = md5.hexdigest
+    end
+    @temp_file = nil # so that the model's original after_save block skips writing to the fs
   end
 
   def is_text?
@@ -155,5 +147,14 @@ class Attachment < ActiveRecord::Base
     df
   end
 
+  def validate
+    if self.filesize > Setting.attachment_max_size.to_i.kilobytes
+      errors.add(:base, :too_long, :count => Setting.attachment_max_size.to_i.kilobytes)
+    end
+  end
+
+  def visible?(user=User.current)
+    container.attachments_visible?(user)
+  end
 end
 
